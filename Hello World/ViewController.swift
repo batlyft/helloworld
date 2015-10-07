2960900de7f5cd64b41cc988a8b7c3bd0ea99ade
Bat Computer <Batcomputer@Bats-MacBook-Pro.local>
2015-10-07 15:36:07 -0700

    first commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -5,14 +5,26 @@
 //  Created by Bat Computer on 9/3/15.
 //  Copyright © 2015 HardSoft. All rights reserved.
 //
+// My first coding assignment with Swift 2!
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
 
+    @IBAction func submit(sender: AnyObject) {
+        
+        print("button tapped")
+        
+        label.text = textField.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello, Jimmy!")
     }
 
     override func didReceiveMemoryWarning() {
